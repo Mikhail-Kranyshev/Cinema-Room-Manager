af848fa58f1fb1a5f3811881a4a9acdb35d91240
Mikhail-Kranyshev <91139884+Mikhail-Kranyshev@users.noreply.github.com>
2022-07-21 16:22:51 +0700

    Stage 4/5: Menu, please!

--- Cinema.java
+++ Cinema.java
@@ -17,7 +17,6 @@ public class Cinema {
             }
             System.out.println("");
         }
-        System.out.println("");
     }
 
     public static char[][] createCinema(int n, int m) {
@@ -25,30 +24,57 @@ public class Cinema {
         for (char[] i: matrix) {
             Arrays.fill(i, 'S');
         }
-        print(matrix);
         return matrix;
     }
 
+    public static int menu(){
+        System.out.println("\n1. Show the seats\n" +
+                "2. Buy a ticket\n" +
+                "0. Exit");
+        int n = -1;
+        while (n < 0 || n > 2) {
+            Scanner sc = new Scanner(System.in);
+            if (sc.hasNextInt()) {
+                n = sc.nextInt();
+            }
+        }
+        return n;
+    }
+
+    public static char[][] buyTicket(char[][] matrix, Scanner sc){
+        System.out.println("Enter a row number:");
+        int rowNumber = sc.nextInt();
+        System.out.println("Enter a seat number in that row:");
+        int seatNumber = sc.nextInt();
+
+        int price = 10;
+        if (matrix.length * matrix[0].length > 60 && rowNumber > matrix.length / 2) {
+            price = 8;
+        }
+        System.out.println("\nTicket price: $" + price);
+        matrix[rowNumber - 1][seatNumber - 1] = 'B';
+        return matrix;
+    }
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Enter the number of rows:");
         int numberOfRows = scanner.nextInt();
         System.out.println("Enter the number of seats in each row:");
         int numberOfSeats = scanner.nextInt();
         char[][] cinema = createCinema(numberOfRows, numberOfSeats);
-
-        System.out.println("Enter a row number:");
-        int rowNumber = scanner.nextInt();
-        System.out.println("Enter a seat number in that row:");
-        int seatNumber = scanner.nextInt();
-
-        int price = 10;
-        if (numberOfRows * numberOfSeats > 60 && rowNumber > numberOfRows / 2) {
-            price = 8;
-        }
-        System.out.println("\nTicket price: $" + price);
-        cinema[rowNumber - 1][seatNumber - 1] = 'B';
-        print(cinema);
+        int choice = 0;
+        do {
+            choice = menu();
+            switch (choice) {
+                case 1:
+                    print(cinema);
+                    break;
+                case 2:
+                    cinema = buyTicket(cinema, scanner);
+                    break;
+            }
+        } while (choice != 0);
     }
 }
